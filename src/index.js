0274500b2024d5f7f9af453a6f45ec4b710787a8
yoann-truchy <yoann.truchy@ytmail.fr>
2026-01-29 13:38:54 +0100

    dockerfile

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const auth = require("./modules/authentication");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/auth/:secret", (req, res) => {
-  const { secret } = req.params;
-  const response = auth(secret);
-
-  res.status(response.status).send(response.message);
 });
 
 app.listen(port, () => {
